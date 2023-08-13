bb524005fabd531819b665e844527ee502c38e62
Lonenso <isxie.yixin@gmail.com>
2023-08-13 13:33:56 +0800

    update windows dotfiles

--- aliases.ps1
+++ aliases.ps1
@@ -12,28 +12,29 @@ ${function:......} = { Set-Location ..\..\..\..\.. }
 # Correct PowerShell Aliases if tools are available (aliases win if set)
 # WGet: Use `wget.exe` if available
 if (Get-Command wget.exe -ErrorAction SilentlyContinue | Test-Path) {
-  rm alias:wget -ErrorAction SilentlyContinue
+  Remove-Item alias:wget -ErrorAction SilentlyContinue
 }
 
 # Directory Listing: Use `ls.exe` if available
 if (Get-Command ls.exe -ErrorAction SilentlyContinue | Test-Path) {
-    rm alias:ls -ErrorAction SilentlyContinue
+    Remove-Item alias:ls -ErrorAction SilentlyContinue
     # Set `ls` to call `ls.exe` and always use --color
-    ${function:ls} = { ls.exe --color @args }
+    ${function:ls} = { Get-ChildItem --color @args }
     # List all files in long format
-    ${function:l} = { ls -lF @args }
+    ${function:l} = { Get-ChildItem -lF @args }
     # List all files in long format, including hidden files
-    ${function:la} = { ls -laF @args }
+    ${function:la} = { Get-ChildItem -laF @args }
     # List only directories
     ${function:lsd} = { Get-ChildItem -Directory -Force @args }
 } else {
     # List all files, including hidden files
-    ${function:la} = { ls -Force @args }
+    ${function:la} = { Get-ChildItem -Force @args }
     # List only directories
     ${function:lsd} = { Get-ChildItem -Directory -Force @args }
 }
 
 Set-Alias which where.exe
+Set-Alias vim gvim
 function ez {
     . $profile
 }
--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,17 +1,15 @@
-echo "sync dotfiles..."
+Write-Output "sync dotfiles..."
+$ErrorActionPreference = "Stop"
 git pull origin main
-$profileDir = Split-Path -parent $profile
-
 function doIt {
     foreach ( $file in ".ideavimrc", ".gitconfig", ".git-commit-template.txt", ".gitignore", ".gitconfig-windows") {
-        sudo New-Item -ItemType SymbolicLink -Path "$home/$file" -Target "$file" -Force
+        New-Item -ItemType SymbolicLink -Path (Join-Path $home $file) -Target (Resolve-Path $file) -Force
     }
-    sudo New-Item -ItemType SymbolicLink -Path "$home/_vimrc" -Target ".vimrc" -Force
+    New-Item -ItemType SymbolicLink -Path (Join-Path $home _vimrc) -Target (Resolve-Path .vimrc) -Force
     foreach ( $file in "profile.ps1", "functions.ps1", "env.ps1", "exports.ps1", "aliases.ps1") {
-        sudo New-Item -ItemType SymbolicLink -Path "$profileDir/$file" -Target "$file" -Force
+        New-Item -ItemType SymbolicLink -Path (Join-Path $PSHOME $file) -Target (Resolve-Path $file) -Force
     }
     Remove-Variable file 
-    & $profile
+    . (Join-Path $PSHOME profile.ps1)
 }
 doIt
-Remove-Variable profileDir
