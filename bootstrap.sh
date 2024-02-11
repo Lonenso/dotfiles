7dc18ee9f3dc67428ac7592c64953d42415be191
Lonenso <isxie.yixin@gmail.com>
2024-02-12 00:05:53 +0800

    update dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ cd "${0/*}" || exit 1
 echo "sync dotfiles..."
 git pull origin main
 function doIt() {
-    for file in .{antigenrc,aliases,exports,functions,gitconfig,gitignore,tmux.conf,vimrc,gvimrc,zshrc,git-commit-template.txt}; do
+    for file in .{antigenrc,aliases,exports,functions,gitconfig,gitignore,tmux.conf,vimrc,gvimrc,zshrc,git-commit-template.txt,clash-config.yaml}; do
         ln -sf "$(pwd)/${file}" "${HOME}/${file}"
     done;
     unset file;
